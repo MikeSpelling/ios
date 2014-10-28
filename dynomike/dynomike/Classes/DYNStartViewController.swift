3a7bdb59e6d641f8c002af17c9ef6e8488b1922c
Mike <mike.spelling@hotmail.co.uk>
2014-10-28 14:08:53 +0000

    Added Alamofire and get use it to print response from local rails server

--- dynomike/dynomike/Classes/DYNStartViewController.swift
+++ dynomike/dynomike/Classes/DYNStartViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Alamofire
 
 class DYNStartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://127.0.0.1:3000/api")
+            .responseString { (_, _, string, _) in
+                println(string)
+        }
     }
 
     override func didReceiveMemoryWarning() {
